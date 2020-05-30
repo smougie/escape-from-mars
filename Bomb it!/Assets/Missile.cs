fc77395630226d388608679666219fde5ad1fd1a
Mateusz Antczak <smougie@gmail.com>
2020-05-30 17:45:47 +0200

    Missile Relative Force

---
+++ Bomb it!/Assets/Missile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        ProccesInput();        
+    }
+
+    private void ProccesInput()
+    {
+        if (Input.GetKey(KeyCode.Space))  // can adjust speed while rotating
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        // reacting only to one statement at time
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+
+        }
+    }
+}
