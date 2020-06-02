28fae33e31c70d67bdfa5696d68323288b5945c6
Mateusz Antczak <smougie@gmail.com>
2020-06-02 02:30:32 +0200

    Audio for thrusting

--- Bomb it!/Assets/Missile.cs
+++ Bomb it!/Assets/Missile.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Missile : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -22,16 +24,38 @@ public class Missile : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))  // can adjust speed while rotating
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            StartCoroutine(FadeOut(audioSource));
         }
 
         // reacting only to one statement at time
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-
+            transform.Rotate(-Vector3.forward);
         }
+    }
+
+    IEnumerator FadeOut(AudioSource audioSource)
+    {
+        float FadeTime = .5f;
+        float startVolume = audioSource.volume;
+        while (audioSource.volume > 0)
+        {
+            audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+            yield return null;
+        }
+        audioSource.Stop();
+        audioSource.volume = startVolume;
+
     }
 }
