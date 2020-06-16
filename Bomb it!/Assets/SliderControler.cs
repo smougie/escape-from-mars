53455ab48012e6ddd930305f28e99eea303df5a0
Mateusz Antczak <smougie@gmail.com>
2020-06-16 15:32:48 +0200

    Bar color change update

---
+++ Bomb it!/Assets/SliderControler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderControler : MonoBehaviour
+{
+    [SerializeField] private Sprite yellowBar;
+    [SerializeField] private Sprite redBar;
+    private Image currentImage;
+
+    void Start()
+    {
+        currentImage = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        if (currentImage.fillAmount <= .3f)
+        {
+            currentImage.sprite = redBar;
+        }
+        else
+        {
+            currentImage.sprite = yellowBar;
+        }
+    }
+}
