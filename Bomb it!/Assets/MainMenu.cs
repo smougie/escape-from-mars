fe80765f44c4fa64adb4d848aa67e61c5e42c2be
Mateusz Antczak <smougie@gmail.com>
2020-07-11 17:53:30 +0200

    MainMenu Update

---
+++ Bomb it!/Assets/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void DisableMainMenu()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void EnableMainMenu()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        // TODO delete print()
+        print("QUIT!");
+    }
+}
