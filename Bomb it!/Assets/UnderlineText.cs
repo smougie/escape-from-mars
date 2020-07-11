fc3434a9b636a3bfd3381075e17d6f864e426b61
Mateusz Antczak <smougie@gmail.com>
2020-07-12 00:29:24 +0200

    MainMenu Underline text update

---
+++ Bomb it!/Assets/UnderlineText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UnderlineText : MonoBehaviour
+{
+    private TMP_Text buttonText;
+    private string currentText;
+
+    void Start()
+    {
+        buttonText = gameObject.GetComponent<TMP_Text>();
+    }
+
+    public void SetUnderlineText()
+    {
+        currentText = buttonText.text;
+        buttonText.text = "<u>" + currentText;
+    }
+
+    public void RemoveUnderlineText()
+    {
+        buttonText = gameObject.GetComponent<TMP_Text>();
+        buttonText.text = currentText;
+    }
+}
