0e1c1d7e8bdda4c28a6c9a4805cc1b1f30311db5
Mateusz Antczak <smougie@gmail.com>
2020-08-04 15:03:27 +0200

    EndLevelWindow - Level Number Label Update

---
+++ Bomb it!/Assets/LevelNumberLabel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LevelNumberLabel : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI levelLabelTxt;
+
+    void Start()
+    {
+        UpdateLevelLabelTxt();
+    }
+
+    private void OnEnable()
+    {
+        UpdateLevelLabelTxt();
+    }
+
+    private void OnDisable()
+    {
+        ClearLevelLabelTxt();
+    }
+
+    private void UpdateLevelLabelTxt()
+    {
+        levelLabelTxt.text = $"Level {SceneManager.GetActiveScene().buildIndex}";
+    }
+
+    private void ClearLevelLabelTxt()
+    {
+        levelLabelTxt.text = "";
+    }
+}
