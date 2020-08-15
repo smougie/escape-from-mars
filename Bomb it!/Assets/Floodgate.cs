201e5ce7d2690addf74ca795048078d55f7c8a2b
Mateusz Antczak <smougie@gmail.com>
2020-08-16 00:05:30 +0200

    Floodgate
    System for opening and closing gate:
    -Open time
    -Closed time
    -Opening speed
    -Closing speed

--- Bomb it!/Assets/Floodgate.cs
+++ Bomb it!/Assets/Floodgate.cs
@@ -8,34 +8,77 @@ public class Floodgate : MonoBehaviour
     [SerializeField] Vector3 targetPosition;
     private Vector3 startPosition;
     private Vector3 endPosition;
-    private float moveSpeed;
-    private float lockedPauseTime;
-    private float openedPauseTime;
+    [SerializeField] float closingSpeed;
+    [SerializeField] float openingSpeed;
+    [SerializeField] float lockedPauseTime;
+    [SerializeField] float openedPauseTime;
+    private bool gateClosed;
+    private bool gateOpened;
 
     void Start()
     {
         startPosition = transform.position;
         AdjustEndPosition();
+        StartMovingGate();
     }
 
-    private void AdjustEndPosition()
+    private void StartMovingGate()
     {
-        if (targetPosition.z != 0)
-        {
-            
-        }
-        if (targetPosition.y != 0)
-        {
-
-        }
-        if (targetPosition.z != 0)
-        {
-
-        }
+        gateClosed = true;
     }
 
     void Update()
     {
-        
+        if (gateClosed)
+        {
+            StartCoroutine(OpenTheGate());
+        }
+        if (gateOpened)
+        {
+            StartCoroutine(CloseTheGate());
+        }
     }
+
+    private void AdjustEndPosition()
+    {
+        endPosition = startPosition;
+        if (targetPosition.z != 0)
+        {
+            endPosition.z = targetPosition.z;
+        }
+        if (targetPosition.y != 0)
+        {
+            endPosition.y = targetPosition.y;
+
+        }
+        if (targetPosition.z != 0)
+        {
+            endPosition.z = targetPosition.z;
+        }
+    }
+
+    IEnumerator OpenTheGate()
+    {
+        gateClosed = false;
+        while (transform.position != endPosition)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPosition, Time.deltaTime * openingSpeed);
+            yield return null;
+        }
+        yield return new WaitForSeconds(openedPauseTime);
+        gateOpened = true;
+    }
+
+    IEnumerator CloseTheGate()
+    {
+        gateOpened = false;
+        while (transform.position != startPosition)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startPosition, Time.deltaTime * closingSpeed);
+            yield return null;
+        }
+        yield return new WaitForSeconds(lockedPauseTime);
+        gateClosed = true;
+    }
+
 }
