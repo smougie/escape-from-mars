409690e5e299c1044856ddf3f14f13c701bbae9b
Mateusz Antczak <smougie@gmail.com>
2020-08-29 23:57:50 +0200

    Rebuilding Refueling Pad system

---
+++ Bomb it!/Assets/EnableLevelObjects.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableLevelObjects : MonoBehaviour
+{
+    [SerializeField] bool disableOnStart;
+    [SerializeField] GameObject[] toEnable;
+
+    void Start()
+    {
+        if (disableOnStart)
+        {
+            DisableGameObjects();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Rocket")
+        {
+            EnableGameObjects();
+        }
+    }
+
+    private void DisableGameObjects()
+    {
+        foreach (var item in toEnable)
+        {
+            item.SetActive(false);
+        }
+    }
+
+    private void EnableGameObjects()
+    {
+        foreach (var item in toEnable)
+        {
+            item.SetActive(true);
+        }
+    }
+}
